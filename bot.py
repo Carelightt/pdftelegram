61028133e2db947a055b0ba46f7e4dd7528ed875
Carelightt <1carelight@gmail.com>
2025-12-19 10:25:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,10 +37,10 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BOT_KEY = os.getenv("BOT_KEY")  # 🔑 siteyle aynı olmalı
 
-PDF_URL = "https://pdf-admin-panel-r523.onrender.com/generate"  # Ücret formu endpoint'i
-KART_PDF_URL = "https://pdf-admin-panel-r523.onrender.com/generate2"
-BURS_PDF_URL = "https://pdf-admin-panel-r523.onrender.com/generate3"  # ✅ Burs endpoint'i (sablon3.pdf)
-DIP_PDF_URL = "https://pdf-admin-panel-r523.onrender.com/diploma"  # ✅ YENİ: Dip endpoint'i (d.pdf)
+PDF_URL = "https://pdfv32026.onrender.com/generate"  # Ücret formu endpoint'i
+KART_PDF_URL = "https://pdfv32026.onrender.com/generate2"
+BURS_PDF_URL = "https://pdfv32026.onrender.com/generate3"  # ✅ Burs endpoint'i (sablon3.pdf)
+DIP_PDF_URL = "https://pdfv32026.onrender.com/diploma"  # ✅ YENİ: Dip endpoint'i (d.pdf)
 
 HEADERS_BASE = {
     "User-Agent": "Mozilla/5.0",
